36291a71d388e13a145afb06153614f039b2c931
GeunSam2 <rootiron96@gmail.com>
2023-09-12 01:04:26 +0900

    feat: edit this page link update
    Signed-off-by: GeunSam2 <rootiron96@gmail.com>

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,5 +2,6 @@ export default {
     logo: <span>ArgoWorkflows-OSS</span>,
     project: {
       link: 'https://github.com/ArgoWorkflows-OSS'
-    }
+    },
+    docsRepositoryBase: 'https://github.com/ArgoWorkflows-OSS/argoworkflows-oss.github.io/tree/main'
   }
