248c25b59405ee8f54b1f83dd30375995aae37b7
GeunSam2 <rootiron96@gmail.com>
2023-09-12 00:20:46 +0900

    feat: support i18n
    Signed-off-by: GeunSam2 <rootiron96@gmail.com>

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,6 +1,10 @@
 export default {
-    logo: <span>ArgoWorkflows-OSS</span>,
-    project: {
-      link: 'https://github.com/ArgoWorkflows-OSS'
-    }
-  }
+  logo: <span>ArgoWorkflows-OSS</span>,
+  project: {
+    link: 'https://github.com/ArgoWorkflows-OSS'
+  },
+  i18n: [
+    { locale: 'ko', text: '한국어' },
+    { locale: 'en', text: 'English' },
+  ]
+}
