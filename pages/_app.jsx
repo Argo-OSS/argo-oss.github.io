7ea9415f51e50129ffda51b9f0c2bfe8a6777f8d
GeunSam2 (Gray) <rootiron96@gmail.com>
2023-09-26 04:09:54 +0900

    feat: favicon and seo (#90)
    Signed-off-by: GeunSam2 <rootiron96@gmail.com>

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,14 @@
+import Head from 'next/head';
 import '../global.css';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- theme.config.jsx
+++ theme.config.jsx
@@ -2,4 +2,15 @@ export default {
   logo: <span>ArgoWorkflows-OSS</span>,
   project: { link: 'https://github.com/ArgoWorkflows-OSS' },
   docsRepositoryBase: 'https://github.com/ArgoWorkflows-OSS/argoworkflows-oss.github.io/tree/main',
+
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="ArgoWorkflows-OSS" />
+      <meta property="og:description" content="The team page of ArgoWorkflows-OSS" />
+      <meta property="og:image" content="https://avatars.githubusercontent.com/u/138947630?s=200&v=4" />
+      <meta property="og:url" content="https://argoworkflows-oss.github.io" />
+      <meta property="og:type" content="website" />
+    </>
+  ),
 };
